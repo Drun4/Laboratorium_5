5fa963779523d8c23d2e11ead3f2b49cea8077b5
Drun4 <isaenkoandrej111@gmail.com>
2022-04-11 01:24:32 +0300

    LEGKOST_BYTIA

--- Laboratorium_5/MainWindow.xaml.cs
+++ Laboratorium_5/MainWindow.xaml.cs
@@ -26,6 +26,9 @@ namespace Laboratorium_5
         double _gogglesPrice;
         double _skiPolesPrice;
 
+        DateTime orderDate;
+        DateTime returnDate;
+
         List<double> sliderValues = new List<double>();
         List<int> kitAmount = new List<int>();
 
@@ -33,10 +36,25 @@ namespace Laboratorium_5
         List<int> secondComboboxIndexes = new List<int>();
         List<int> thirdComboboxIndexes = new List<int>();
 
+        List<DateTime> orderDates = new List<DateTime>();
+        List<DateTime> returnDates = new List<DateTime>();
+
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new ComboboxViewModel();
+        }
+
+        public int dateCompare(DateTime firstDate, DateTime secondDate)
+        {
+            if (orderDate > returnDate)
+            {
+                return 0;
+            }
+            else
+            {
+                return (returnDate - orderDate).Days;
+            }
         }
 
         private void btn_add_Click(object sender, RoutedEventArgs e)
@@ -51,72 +69,118 @@ namespace Laboratorium_5
             }
             else
             {
-                try
+                if(dateCompare(orderDate, returnDate) == 0)
                 {
-                    if(cmbx_addHelmet.SelectedIndex == 0)
-                    {
-                        _helmetPrice = addElementPrice;
-                    }
-                    else
-                    {
-                        _helmetPrice = 0;
-                    }
-                    if (cmbx_addGoggle.SelectedIndex == 0)
-                    {
-                        _gogglesPrice = addElementPrice;
-                    }
-                    else
-                    {
-                        _gogglesPrice = 0;
-                    }
-                    if (cmbx_addSki.SelectedIndex == 0)
-                    {
-                        _skiPolesPrice = addElementPrice;
-                    }
-                    else
-                    {
-                        _skiPolesPrice = 0;
-                    }
-                    Order order = new Order(Math.Round(Convert.ToDouble(slider_kitPrice.Value), 1),
-                                            Convert.ToInt32(txt_kitAmount.Text), _helmetPrice, _gogglesPrice, _skiPolesPrice);
-                    Person person = new Person(txt_name.Text, txt_surname.Text, order.getFinalPrice);
-
-                    lstbx_people.Items.Add(person);
-                    kitAmount.Add(order.kitAmount);
-                    sliderValues.Add(order.kitPrice);
-
-                    firstComboboxIndexes.Add(cmbx_addHelmet.SelectedIndex);
-                    secondComboboxIndexes.Add(cmbx_addGoggle.SelectedIndex);
-                    thirdComboboxIndexes.Add(cmbx_addSki.SelectedIndex);
+                    MessageBox.Show("Error in dates!");
                 }
-                catch (FormatException)
+                else
                 {
-                    MessageBox.Show("Format error!");
+                    try
+                    {
+                        if (cmbx_addHelmet.SelectedIndex == 0)
+                        {
+                            _helmetPrice = addElementPrice;
+                        }
+                        else
+                        {
+                            _helmetPrice = 0;
+                        }
+                        if (cmbx_addGoggle.SelectedIndex == 0)
+                        {
+                            _gogglesPrice = addElementPrice;
+                        }
+                        else
+                        {
+                            _gogglesPrice = 0;
+                        }
+                        if (cmbx_addSki.SelectedIndex == 0)
+                        {
+                            _skiPolesPrice = addElementPrice;
+                        }
+                        else
+                        {
+                            _skiPolesPrice = 0;
+                        }
+                        Order order = new Order(Math.Round(Convert.ToDouble(slider_kitPrice.Value), 1), Convert.ToInt32(txt_kitAmount.Text),
+                                                    dateCompare(orderDate, returnDate), _helmetPrice, _gogglesPrice, _skiPolesPrice);
+                        Person person = new Person(txt_name.Text, txt_surname.Text, order.getFinalPrice);
+
+                        lstbx_people.Items.Add(person);
+
+                        kitAmount.Add(order.kitAmount);
+                        sliderValues.Add(order.kitPrice);
+
+                        firstComboboxIndexes.Add(cmbx_addHelmet.SelectedIndex);
+                        secondComboboxIndexes.Add(cmbx_addGoggle.SelectedIndex);
+                        thirdComboboxIndexes.Add(cmbx_addSki.SelectedIndex);
+
+                        orderDates.Add(orderDate);
+                        returnDates.Add(returnDate);
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("Format error!");
+                    }
                 }
             }
         }
-
-        private void orderDate_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if(orderDate.DisplayDate < DateTime.Today)
-            {
-                MessageBox.Show("Mistakes in the entered information!");
-            }
-        }
+        //if(cal_orderDate.DisplayDate<DateTime.Today)
+        //    {
+        //        MessageBox.Show("Mistakes in the entered information!");
+        //    }
 
         private void lstbx_people_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             int personIndex = lstbx_people.SelectedIndex;
 
-            Person tempPerson = (Person)lstbx_people.Items[personIndex];
-            txt_name.Text = tempPerson.name;
-            txt_surname.Text = tempPerson.surname;
-            txt_kitAmount.Text = Convert.ToString(kitAmount[personIndex]);
-            slider_kitPrice.Value = sliderValues[personIndex];
+            if (lstbx_people.SelectedIndex >= 0)
+            {
+                Person tempPerson = (Person)lstbx_people.Items[personIndex];
+                txt_name.Text = tempPerson.name;
+                txt_surname.Text = tempPerson.surname;
+                txt_kitAmount.Text = Convert.ToString(kitAmount[personIndex]);
+                slider_kitPrice.Value = sliderValues[personIndex];
 
-            cmbx_addHelmet.SelectedIndex = firstComboboxIndexes[personIndex];
-            cmbx_addGoggle.SelectedIndex = secondComboboxIndexes[personIndex];
-            cmbx_addSki.SelectedIndex = thirdComboboxIndexes[personIndex];
+                cmbx_addHelmet.SelectedIndex = firstComboboxIndexes[personIndex];
+                cmbx_addGoggle.SelectedIndex = secondComboboxIndexes[personIndex];
+                cmbx_addSki.SelectedIndex = thirdComboboxIndexes[personIndex];
+
+                cal_orderDate.SelectedDate = orderDates[personIndex];
+                cal_returnDate.SelectedDate = returnDates[personIndex];
+            }
+        }
+
+        private void cal_orderDate_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            orderDate = (DateTime)cal_orderDate.SelectedDate;
+        }
+
+        private void cal_returnDate_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            returnDate = (DateTime)cal_returnDate.SelectedDate;
+        }
+
+        private void btn_removeSelected_Click(object sender, RoutedEventArgs e)
+        {
+            int personIndex = lstbx_people.SelectedIndex;
+            if (lstbx_people.SelectedIndex >= 0)
+            {
+                lstbx_people.Items.RemoveAt(personIndex);
+
+                kitAmount.RemoveAt(personIndex);
+                sliderValues.RemoveAt(personIndex);
+
+                firstComboboxIndexes.RemoveAt(personIndex);
+                secondComboboxIndexes.RemoveAt(personIndex);
+                thirdComboboxIndexes.RemoveAt(personIndex);
+
+                orderDates.RemoveAt(personIndex);
+                returnDates.RemoveAt(personIndex);
+            }
+            else
+            {
+                MessageBox.Show("Choose a person!");
+            }
         }
     }
 }
--- Laboratorium_5/Order.cs
+++ Laboratorium_5/Order.cs
@@ -4,15 +4,17 @@
     {
         public double kitPrice { get; set; }
         public int kitAmount { get; set; }
+        public int dayAmount { get; set; }
 
         public double helmetPrice { get; set; }
         public double gogglesPrice { get; set; }
         public double skiPolesPrice { get; set; }
 
-        public Order(double kitPrice, int kitAmount, double helmetPrice, double gogglesPrice, double skiPolesPrice)
+        public Order(double kitPrice, int kitAmount, int dayAmount, double helmetPrice, double gogglesPrice, double skiPolesPrice)
         {
             this.kitAmount = kitAmount;
             this.kitPrice = kitPrice;
+            this.dayAmount = dayAmount;
             this.helmetPrice = helmetPrice;
             this.gogglesPrice = gogglesPrice;
             this.skiPolesPrice = skiPolesPrice;
@@ -22,7 +24,7 @@
         {
             get
             {
-                return kitAmount * (kitPrice + helmetPrice + gogglesPrice + skiPolesPrice);
+                return dayAmount * kitAmount * (kitPrice + helmetPrice + gogglesPrice + skiPolesPrice);
             }
         }
     }
